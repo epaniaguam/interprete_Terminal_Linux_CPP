d449f9a87eb235440fc54808ac9c7661e43fc1a4
Elian <93670966+epaniaguam@users.noreply.github.com>
2023-12-14 22:59:33 -0500

    Identificacion de SUDO

--- BetaProducto01.cpp
+++ BetaProducto01.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <regex>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <string>
+using namespace std;
 
+string extraerSudo(string &input)
+{
+    string modoSudo = "";
+    if (input.find("sudo") == 0)
+    {
+        modoSudo = "sudo";
+        input = input.substr(5, input.length());
+    }
+    return modoSudo;
+}
+
+int main()
+{
+  inputComando = "/bin/ cp -r /directorio /copia_directorio > ~/Dodumentos/texto.txt";
+  string inputComando;
+  string modoSudo;
+  modoSudo = extraerSudo(inputComando); // aun sin usar
+  
+}
