92ab87d4844ff737f768242a509318f735237b72
Elian <93670966+epaniaguam@users.noreply.github.com>
2023-12-14 23:42:58 -0500

    Beta con regex terminal

--- BetaProducto01.cpp
+++ BetaProducto01.cpp
@@ -2,11 +2,16 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <regex>
-#include <cstdio>
-#include <cstdlib>
+#include <cstdio> // opcional
+#include <cstdlib> // Para 
 #include <vector>
 #include <string>
+
 using namespace std;
+
+
+//  (~|\.|\/)*(?:[a-zA-Z0-9_-]+\/?)+(\.[a-zA-Z0-9]+)+     Detecta archivos con directorio y extension, directorio opcional
+//  (~|(\.\/)|\/)+(?:[a-zA-Z0-9_-]+\/?)+
 
 string extraerSudo(string &input)
 {
@@ -197,10 +202,9 @@ string dosArgumentosConEspacio(string &input){
     }
 }
 
-
-
 int main()
 {
+
     string inputComando;
     
     //inputComando = "/bin/ ls -lsa / > ~/Descargas/salida.txt";
@@ -213,83 +217,90 @@ int main()
 
     //cout << "▒ESIS $ ";
     //getline(cin,inputComando);
-    
-    string modoSudo,ruta,comando, argumentos,redireccion,archivo, rutaComando, dosArgumentos,argOrigen,argDestino;
-    modoSudo = extraerSudo(inputComando); // aun sin usar
-    ruta = extraerRuta(inputComando);
-        
-    dosArgumentos = dosArgumentosConEspacio(inputComando); //funcion para capturar dos argumentos con espacio
-    argOrigen = dosArgumentos.substr(0,dosArgumentos.find(" "));
-    argDestino = dosArgumentos.substr(dosArgumentos.find(" ")+1,dosArgumentos.length());
+
+    while (inputComando != "salir")
+    {
+        string modoSudo,ruta,comando, argumentos,redireccion,archivo, rutaComando, dosArgumentos,argOrigen,argDestino;
+        modoSudo = extraerSudo(inputComando); // aun sin usar
+        ruta = extraerRuta(inputComando);
+
+        dosArgumentos = dosArgumentosConEspacio(inputComando); //funcion para capturar dos argumentos con espacio
+        argOrigen = dosArgumentos.substr(0,dosArgumentos.find(" "));
+        argDestino = dosArgumentos.substr(dosArgumentos.find(" ")+1,dosArgumentos.length());
 
 
-    comando = extraerComando(inputComando);
-    argumentos = extraerArgumentos(inputComando);
-    rutaComando = extraerRutacomando(inputComando);
-    reconocerHomeUser(rutaComando);
+        comando = extraerComando(inputComando);
+        argumentos = extraerArgumentos(inputComando);
+        rutaComando = extraerRutacomando(inputComando);
 
-    redireccion = ""; archivo = "";
-    //captura redireccion y archivo si es que hay esos elementos en el inputComando
-    //caso contrario los mantiene en vacio
-    extraerRedireccion(inputComando,redireccion,archivo); 
-    reconocerHomeUser(archivo);
+        reconocerHomeUser(rutaComando);
 
-    if (redireccion == ">" && archivo != "") {
-        int pidRedireccion = fork();
-        if (pidRedireccion == 0) { // proceso hijo para la redirección
-            // Configura la redirección
-            FILE* outputFile = freopen(archivo.c_str(), "w", stdout);
-            if (outputFile == nullptr) {
-                perror("freopen");
+        redireccion = ""; archivo = "";
+        //captura redireccion y archivo si es que hay esos elementos en el inputComando
+        //caso contrario los mantiene en vacio
+        extraerRedireccion(inputComando,redireccion,archivo); 
+        reconocerHomeUser(archivo);
+
+        if (redireccion == ">" && archivo != "") {
+            int pidRedireccion = fork();
+            if (pidRedireccion == 0) { // proceso hijo para la redirección
+                // Configura la redirección
+                FILE* outputFile = freopen(archivo.c_str(), "w", stdout);
+                if (outputFile == nullptr) {
+                    perror("freopen");
+                    return 1;
+                }
+
+                // Ejecuta el comando
+                ruta += comando;
+
+                if(!rutaComando.empty() && !argumentos.empty()){
+                    execl(ruta.c_str(), comando.c_str(), argumentos.c_str(),rutaComando.c_str(), NULL);
+                }else if (!argumentos.empty() && !dosArgumentos.empty()) {
+                    execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
+                }else if (!dosArgumentos.empty()) {
+                    execl(ruta.c_str(), comando.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
+                }else if (!argumentos.empty()) {
+                    execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), NULL);
+                }else if (!rutaComando.empty()){
+                    execl(ruta.c_str(), comando.c_str(), rutaComando.c_str(), NULL);
+                } else {
+                    execl(ruta.c_str(), comando.c_str(), NULL);
+                }
+
+                perror("execl");
                 return 1;
+            } else {
+                waitpid(pidRedireccion, NULL, 0); // Espera a que el hijo termine
             }
-
-            // Ejecuta el comando
+        } else {
+            // Ejecuta el comando sin redirección
             ruta += comando;
-
-            if(!rutaComando.empty() && !argumentos.empty()){
-                execl(ruta.c_str(), comando.c_str(), argumentos.c_str(),rutaComando.c_str(), NULL);
-            }else if (!argumentos.empty() && !dosArgumentos.empty()) {
-                execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
-            }else if (!dosArgumentos.empty()) {
-                execl(ruta.c_str(), comando.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
-            }else if (!argumentos.empty()) {
-                execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), NULL);
-            }else if (!rutaComando.empty()){
-                execl(ruta.c_str(), comando.c_str(), rutaComando.c_str(), NULL);
+            int pid = fork();
+            if (pid == 0) { // proceso hijo
+                if(!rutaComando.empty() && !argumentos.empty()){
+                    execl(ruta.c_str(), comando.c_str(), argumentos.c_str(),rutaComando.c_str(), NULL);
+                }else if (!argumentos.empty() && !dosArgumentos.empty()) {
+                    execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
+                }else if (!dosArgumentos.empty()) {
+                    execl(ruta.c_str(), comando.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
+                }else if (!argumentos.empty()) {
+                    execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), NULL);
+                }else if (!rutaComando.empty()){
+                    execl(ruta.c_str(), comando.c_str(), rutaComando.c_str(), NULL);
+                } else {
+                    execl(ruta.c_str(), comando.c_str(), NULL);
+                }
+                perror("execl");
+                return 1;
             } else {
-                execl(ruta.c_str(), comando.c_str(), NULL);
+                wait(0);
             }
+        }
 
-            perror("execl");
-            return 1;
-        } else {
-            waitpid(pidRedireccion, NULL, 0); // Espera a que el hijo termine
-        }
-    } else {
-        // Ejecuta el comando sin redirección
-        ruta += comando;
-        int pid = fork();
-        if (pid == 0) { // proceso hijo
-            if(!rutaComando.empty() && !argumentos.empty()){
-                execl(ruta.c_str(), comando.c_str(), argumentos.c_str(),rutaComando.c_str(), NULL);
-            }else if (!argumentos.empty() && !dosArgumentos.empty()) {
-                execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
-            }else if (!dosArgumentos.empty()) {
-                execl(ruta.c_str(), comando.c_str(), argOrigen.c_str(),argDestino.c_str(), NULL);
-            }else if (!argumentos.empty()) {
-                execl(ruta.c_str(), comando.c_str(), argumentos.c_str(), NULL);
-            }else if (!rutaComando.empty()){
-                execl(ruta.c_str(), comando.c_str(), rutaComando.c_str(), NULL);
-            } else {
-                execl(ruta.c_str(), comando.c_str(), NULL);
-            }
-            perror("execl");
-            return 1;
-        } else {
-            wait(0);
-        }
+        cout << "▒ESIS $ ";
+        getline(cin,inputComando);
     }
 
-
+    return 0;
 }
